3d7a95ffe59724a94dd8e2dc308569847415e407
Shamil Omarov <s_omarov05@mail.ru>
2022-06-02 17:34:19 +0300

    Сделал инпут для поиска гифок, во 2 компоненте

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
+import SearchContainer from './components/SearchContainer';
 
 function App() {
   return (
     <div className="app">
       <div className='main'>
         <Header />
-        <h1>Search Container</h1>
+        <SearchContainer />
         <h1>Media</h1>
       </div>
     </div>
