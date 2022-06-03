d62a30cb27f01c3597271af2e52381e20e1d567d
Shamil Omarov <s_omarov05@mail.ru>
2022-06-03 13:48:28 +0300

    Закончил верстку проекта

--- src/components/StoriesGiphySection.js
+++ src/components/StoriesGiphySection.js
@@ -12,8 +12,15 @@ const StoriesGiphySection = ({ giphysArray }) => {
     }
   }
 
+  const getColoredBorders = () => {
+    const colors = ["purple", "sunshine", "bluepurple", "turqoise"];
+    let randomIndex = Math.floor(Math.random() * 4) + 0;
+    return colors[randomIndex];
+  }
+
   const GiphyTile = ({ giphy }) => {
     let giphyURL = giphy ? giphy.images.downsized.url : "";
+    let colorBorders = getColoredBorders();
 
     return (
       <div className="tile">
@@ -22,40 +29,43 @@ const StoriesGiphySection = ({ giphysArray }) => {
             <p>{giphy?.title}</p>
           </div>
           <img src={giphyURL} alt="" />
+          <div className={`overlay-${colorBorders}`}></div>
+        </div>
+        <div className="line-box">
+          <div className={`line-top-${colorBorders}`}></div>
+          <div className={`line-middle-${colorBorders}`}></div>
+          <div className={`line-bottom-${colorBorders}`}></div>
         </div>
       </div>
-    )
+    );
+  };
+
+  const gridGiphysConfig = [
+    ["landscape-left-row", 3],
+    ["landscape-right-row", 3],
+    ["no-landscape-row", 4],
+    ["landscape-middle-row", 3],
+    ["no-landscape-row", 4],
+  ]
+
+  const createTiles = (numTiles) => {
+    let tiles = [];
+    for (let i = 0; i < numTiles; i++) {
+      tiles.push(<GiphyTile giphy={getGiphy()} key={i}/>);
+    }
+    return tiles;
   }
 
   return (
     <div className="stories-section">
-      <div className="landscape-left-row">
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-      </div>
-      <div className="landscape-right-row">
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-      </div>
-      <div className="no-landscape-row">
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-      </div>
-      <div className="landscape-middle-row">
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-      </div>
-      <div className="no-landscape-row">
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-        <GiphyTile giphy={getGiphy()} />
-      </div>
+      {gridGiphysConfig.map(([layoutClass, numTiles], index) => {
+        const tiles = createTiles(numTiles)
+        return (
+          <div className={layoutClass} key={index}>
+            {tiles}
+          </div>
+        )
+      })}
     </div>
   )
 }
